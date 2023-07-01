22e085f6cfd554e21cfb66a7a4cc733846b06323
OlegSsSs <olegs934@gmail.com>
2023-07-01 15:50:22 +0300

    done

--- Toy.java
+++ Toy.java
@@ -1,39 +1,57 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Toy {
+    private static final AtomicInteger COUNTER = new AtomicInteger(0);                     // id игрушки
+    private final String name;                // название
+    private final int id;
+    private int quantity;               // количество
+    private double frequency;           // частота выпадения
 
-    private int id;
-    private String name;
-    private int drop;
 
-    public Toy(int id, String name,int drop){
-        this.id = id;
-        this.name = name;
-        this.drop = drop;
+    @Override
+    public String toString() {
+        return "Toy{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", quantity=" + quantity +
+                ", frequency=" + frequency +
+                '}';
     }
+
+    public Toy(String name, int quantity, double frequency) {
+        this.id = COUNTER.getAndIncrement();
+        this.name = name;
+        this.quantity = quantity;
+        this.frequency = frequency;
+    }
+
 
     public int getId() {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
 
-    public int getDrop() {
-        return drop;
-    }
-
-    public void setDrop(int drop) {
-        this.drop = drop;
+    public int getQuantity() {
+        return quantity;
     }
 
 
+    public double getFrequency() {
+        return frequency;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+
+    public void setFrequency(double frequency) {
+        this.frequency = frequency;
+    }
 
 }
--- ToyList.java
+++
@@ -1,9 +0,0 @@
-import java.util.PriorityQueue;
-
-public class ToyList {
-    PriorityQueue<Toy> toyList = new PriorityQueue<>();
-
-    public void addToQueu(Toy toy) {
-        toyList.add(toy);
-    }
-}
